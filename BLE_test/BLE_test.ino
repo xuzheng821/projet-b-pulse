3ec1fbbd2275a9b05e01a79c84dd79ec7398f1fb
yuanzhoucq <yuanzhou.cq@gmail.com>
2018-01-29 17:41:16 +0100

    Reorganize

---
+++ BLE_test/BLE_test.ino
@@ -0,0 +1,60 @@
+/*
+The sketch demonstrates how to do accept a Bluetooth Low Energy 4
+Advertisement connection with the RFduino, then send CPU temperature
+updates once a second.
+
+It is suppose to be used with the rfduinoTemperature iPhone application.
+*/
+
+/*
+ Copyright (c) 2014 OpenSourceRF.com.  All right reserved.
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <RFduinoBLE.h>
+
+void setup() {
+  // this is the data we want to appear in the advertisement
+  // (if the deviceName and advertisementData are too long to fix into the 31 byte
+  // ble advertisement packet, then the advertisementData is truncated first down to
+  // a single byte, then it will truncate the deviceName)
+  RFduinoBLE.advertisementData = "B-Pulse";
+
+  // start the BLE stack
+  RFduinoBLE.begin();
+}
+
+void loop() {
+  // sample once per second
+  RFduino_ULPDelay( SECONDS(1) );
+}
+void RFduinoBLE_onReceive(char *data, int len){
+  uint8_t m = 0;
+  for(int i=0;i<len;i++){
+    uint8_t myByte = data[i];
+    if(m<myByte) m = myByte;
+  }
+  RFduinoBLE.sendByte(m);
+}
+
